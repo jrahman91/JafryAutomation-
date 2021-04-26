32102a868efef8d494441b7f36f95f198d57fcca
jrahman91 <Jafry.Rahman@gmail.com>
2021-04-26 12:46:33 -0400

    added rahulShetty1.java

--- rahulShetty1.java
+++ rahulShetty1.java
@@ -8,56 +8,33 @@ import org.openqa.selenium.support.ui.Select;
 import org.testng.Assert;
 
 public class rahulShetty1 {
-		
 
 	public static void main(String[] args) throws InterruptedException {
-		
-			
-		System.setProperty("webdriver.chrome.driver", "/Users/jafry.rahman/Desktop/DevWorkSpace/javaAutomation/Drivers/chromedriver");
-		WebDriver driver = new ChromeDriver();
-		
-        String URL= "https://rahulshettyacademy.com/angularpractice/";
-		
-		driver.get(URL);
-		
-		driver.findElement(By.xpath("//input[@name=\"name\"]")).sendKeys("Jafry");
-	
 
-		
+		System.setProperty("webdriver.chrome.driver",
+				"/Users/jafry.rahman/Desktop/DevWorkSpace/javaAutomation/Drivers/chromedriver");
+		WebDriver driver = new ChromeDriver();
+
+		String URL = "https://rahulshettyacademy.com/angularpractice/";
+
+		driver.get(URL);
+
+		driver.findElement(By.xpath("//input[@name=\"name\"]")).sendKeys("Jafry");
+
 		driver.findElement(By.xpath("//input[@name=\"email\"]")).sendKeys("jafryr@Yahoo.com");
-		
+
 		driver.findElement(By.xpath("//input[@id=\"exampleInputPassword1\"]")).sendKeys("Test123");
-		
+
 		driver.findElement(By.xpath("//input[@id=\"exampleCheck1\"]")).click();
-		
+
 		WebElement staticDropdown = driver.findElement(By.id("exampleFormControlSelect1"));
-		Select dropdown= new Select(staticDropdown);
+		Select dropdown = new Select(staticDropdown);
 		dropdown.selectByIndex(0);
 		System.out.println(dropdown.getFirstSelectedOption().getText());
-		
-		driver.findElement(By.xpath("//input[@id=\"inlineRadio2\"]")).click();
-		
-		driver.findElement(By.xpath("//input[@name=\"bday\"]")).sendKeys("01/30/1991");
-		
-		
-	
-	
 
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
+		driver.findElement(By.xpath("//input[@id=\"inlineRadio2\"]")).click();
+
+		driver.findElement(By.xpath("//input[@name=\"bday\"]")).sendKeys("01/30/1991");
 
 	}
 
