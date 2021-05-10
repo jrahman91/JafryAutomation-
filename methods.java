bba28337132a65d1f08580bf4ab9988121e3c82b
jrahman91 <Jafry.Rahman@gmail.com>
2021-05-10 19:36:50 -0400

    added methods.java

--- methods.java
+++ methods.java
@@ -1,56 +1,48 @@
-package javaAutomation;
-
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 
 public class methods {
+	
+	
+	//METHODS
 
-	static void myMethod() {}
-		
-	/*
-		System.out.println("Hello this is a method");
+		static void firstName(String fName) {
+			System.out.println(fName + " " + "Rahman");
+		}
+
+		static void ageAndName(String firstName, int age) {
+			System.out.println(firstName + " is " + age);
+		}
+
+		static int number(int x, int y) {
+			return x + y;
+		}
+
+		static void checkAge(int age) {
+			if (age < 18) {
+				System.out.println("Access Denied- You Are Not Old Enough");
+			} else {
+				System.out.println("Acess Granted");
 			}
-	
-	static void myInfo(String fName, String lName, int age) {
-		System.out.println("My name is " + fName + " " + lName + " and my age is " + age);
-		
-	}
-	
-	
-	*/
-	
-	
-	
+		}
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.setProperty("webdriver.chrome.driver", "/Users/jafry.rahman/Desktop/Drivers/chromedriver");
-		WebDriver driver= new ChromeDriver();
-		
-		//myMethod();
-		//myInfo("jafry", "Rahman", 30);
-		
-		String URL = "https://www.google.com/gmail/about/#";
-		driver.get(URL);
-		
-		String expectedTitle = "Gmail - Email from Google";
-		
-		String actualTitle = driver.getTitle();
-		
-		if (expectedTitle.equals(actualTitle)) { System.out.println("Verification successfull"); 
-		
-		} else {System.out.println("verification failed"); }
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
+		/*
+		 * firstName("Jafry"); 
+		 * firstName("Nate"); firstName("Adam"); 
+		 * firstName("Nick");
+		 */
+
+		ageAndName("Jafry", 30);
+		ageAndName("Nate", 31);
+		ageAndName("Adam", 29);
+		ageAndName("Nick", 29);
+
+		System.out.println(number(4, 4));
+
+		checkAge(19);
+		checkAge(17);
+		checkAge(30);
 
 	}
 
