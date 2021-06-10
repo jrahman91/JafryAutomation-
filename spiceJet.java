02759ffa340387e2faa1b9097ed28aed6abe20c4
jrahman91 <Jafry.Rahman@gmail.com>
2021-06-10 11:08:30 -0400

    added spiceJet.java

--- spiceJet.java
+++ spiceJet.java
@@ -1,5 +1,7 @@
 package javaAutomation;
 
+import java.util.concurrent.TimeUnit;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -9,66 +11,42 @@ import org.openqa.selenium.support.ui.Select;
 public class spiceJet {
 
 	public static void main(String[] args) throws InterruptedException {
-		System.setProperty("webdriver.chrome.driver",
-				"/Users/jafry.rahman/Desktop/DevWorkSpace/javaAutomation/Drivers/chromedriver");
+		
+		System.setProperty("webdriver.chrome.driver", "/Users/jafry.rahman/Desktop/Drivers/chromedriver");
 		WebDriver driver = new ChromeDriver();
 		
-		String URL="https://www.spicejet.com/";
+		String url= "https://www.spicejet.com/";
 		
-		driver.get(URL);
-		Thread.sleep(1000);
+		driver.get(url); 
+		driver.manage().timeouts().implicitlyWait(2,TimeUnit.SECONDS);
 		
 		driver.manage().window().maximize();
 		
-		driver.findElement(By.xpath("//input[@id=\"ctl00_mainContent_rbtnl_Trip_1\"]")).click();
-		System.out.println(driver.findElement(By.xpath("//input[@id=\"ctl00_mainContent_rbtnl_Trip_1\"]")).isEnabled());
+		driver.findElement(By.xpath("//span[@id=\"ctl00_mainContent_ddl_originStation1_CTXTaction\"]")).click();
+		
+		driver.findElement(By.xpath("//a[@value= \"BLR\"]")).click();
+		
+		driver.findElement(By.xpath("//span[@id=\"ctl00_mainContent_ddl_destinationStation1_CTXTaction\"]")).click();
+		
+		driver.findElement(By.xpath("//a[@value=\"MAA\"][1]")).click();
+		System.out.println(driver.findElement(By.xpath("//a[@value=\"MAA\"][1]")).getText());
 		
 				
-		
-		driver.findElement(By.xpath("//input[@id=\"ctl00_mainContent_ddl_originStation1_CTXT\"]")).click();
-		
-		driver.findElement(By.xpath("//a[@value=\"BLR\"]")).click();
-		
-		driver.findElement(By.xpath("//input[@id=\"ctl00_mainContent_ddl_destinationStation1_CTXT\"]")).click();
-		
-		driver.findElement(By.xpath("(//a[@value=\"MAA\"])[1]")).click();
-		System.out.println(driver.findElement(By.xpath("(//a[@value=\"MAA\"])[1]")).getText());
-		
-		
 		driver.findElement(By.xpath("//input[@id=\"ctl00_mainContent_chk_SeniorCitizenDiscount\"]")).click();
+		System.out.println(driver.findElement(By.xpath("//input[@id=\"ctl00_mainContent_chk_SeniorCitizenDiscount\"]")).isEnabled());
 		
-		System.out.println(driver.findElement(By.xpath("//input[@id=\"ctl00_mainContent_chk_SeniorCitizenDiscount\"]")).isSelected());
-		
-		Thread.sleep(1000);
 		WebElement staticDropdown = driver.findElement(By.id("ctl00_mainContent_DropDownListCurrency"));
 		Select dropdown = new Select(staticDropdown);
-		dropdown.selectByIndex(5);
-		
-		Thread.sleep(1000);
+		dropdown.selectByIndex(2);
 		
 		driver.findElement(By.xpath("//input[@id=\"ctl00_mainContent_btn_FindFlights\"]")).click();
+		driver.manage().timeouts().implicitlyWait(2,TimeUnit.SECONDS);
 		
-		 
-		
-		
-		
+		driver.quit();
 		
 		
 		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
+				
 		
 		
 		
