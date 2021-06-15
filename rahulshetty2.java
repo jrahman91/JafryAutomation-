3f1dcb120acc308100e2816e24bd57f39ff801d0
jrahman91 <Jafry.Rahman@gmail.com>
2021-06-15 16:59:01 -0400

    added rahulshetty2.java

--- rahulshetty2.java
+++ rahulshetty2.java
@@ -1,10 +1,15 @@
 package javaAutomation;
 
+import java.util.concurrent.TimeUnit;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.Select;
+import org.testng.Assert;
+
+
 
 public class rahulshetty2 {
 
@@ -15,46 +20,69 @@ public class rahulshetty2 {
 		System.setProperty("webdriver.chrome.driver", "/Users/jafry.rahman/Desktop/Drivers/chromedriver");
 
 		WebDriver driver = new ChromeDriver();
+		
 		String URL= "https://www.rahulshettyacademy.com/AutomationPractice/";
 		driver.get(URL);
 		
+		driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
+		
 		driver.manage().window().maximize();
-		Thread.sleep(1500);		
-				
+		driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
+		
 		driver.findElement(By.xpath("//input[@value=\"radio2\"]")).click();
+		driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
 		
-		driver.findElement(By.xpath("//input[@id= \"autocomplete\"]")).sendKeys("USA");
+		driver.findElement(By.xpath("//input[@id=\"autocomplete\"]")).sendKeys("USA");
+		driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
 		
-		Thread.sleep(1500);
-		
-		//DROPDOWN MENU
-		WebElement staticDropdown = driver.findElement(By.id("dropdown-class-example"));
-		Select dropdown = new Select(staticDropdown);
+		WebElement staticDropdown= driver.findElement(By.id("dropdown-class-example"));
+		Select dropdown= new Select(staticDropdown);
 		dropdown.selectByIndex(2);
+		//System.out.println(driver.findElement(By.id("dropdown-class-example")).getText());
 		System.out.println(dropdown.getFirstSelectedOption().getText());
+		driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
 		
-		Thread.sleep(1500);
-		
-	
-		driver.findElement(By.id("checkBoxOption2")).click();
-		
-		
-		//driver.findElement(By.xpath("//button[@id=\"openwindow\"]")).click();
-		//Thread.sleep(1500);
-		
-		//this code closes the first window, and not the new window that popped up
-		//driver.close();
+		driver.findElement(By.xpath("//input[@id=\"checkBoxOption3\"]")).click();
 		
 		driver.findElement(By.xpath("//input[@id=\"name\"]")).sendKeys("Jafry");
 		
-		driver.findElement(By.xpath("//input[@id= \"hide-textbox\"]")).click();
-		
-		Thread.sleep(1500);
-		
 		driver.findElement(By.xpath("//input[@id=\"alertbtn\"]")).click();
+		//driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
+		Thread.sleep(2000);
 		
-		Thread.sleep(1000);
-		driver.switchTo().alert().accept();
+		driver.switchTo().alert().dismiss();
+		Thread.sleep(2000);
+		
+		driver.close();
+		
+		
+		
+		
+		
+		
+	
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	
+		
+		
+		
+		
+		
+		
+		
+		 
+	
 		
 
 	}
