509acd82087265fe61c8769165d1210c8543b65e
jrahman91 <Jafry.Rahman@gmail.com>
2021-06-14 21:05:48 -0400

    added arraysVSArrayList.java

--- arraysVSArrayList.java
+++ arraysVSArrayList.java
@@ -1,65 +1,136 @@
 package javaAutomation;
 
-public class arraysVSArrayList {
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 
+public class arraysVSArrayList {
+	
+	
 	public static void main(String[] args) {
 		
 		//Arrays are used to store multiple values in a single variable, instead of declaring separate variables for each value.
 		
+		/*
 		
+		String city [] = {"NYC", "Miami", "LA", "Toronto","Austin"};
 		
-		 String [] cars= {"BMW", "Mercedes", "Audi", "Porsche", "Bentley"};
-		 
+		System.out.println(city.length);
 		
-		System.out.println(cars[3]);
-						
-		cars [2]= "Tesla";
+		city [4]= "Dallas";
+		System.out.println(city [4]);
 		
-		System.out.println(cars[2]);
+		//To print out the array, use for-each loop
 		
-		System.out.println(cars.length);	
-		
-		//LOOPIING THROUGH ARRAY
-		
-		for (int x=0; x< cars.length; x++) {
-			System.out.println(cars[x]);
+		for (String y: city) {
+			System.out.println(y);
 		}
 		
+		if (city[1].equals("LA")) {System.out.println("Index 1 is not LA");
 		
-        
-		if (cars[3].equals("Porsche")) {System.out.println("Index 3 is Porsche"); }
-		else {System.out.println("Index 3 is not porsche"); }
+		} else System.out.println("Index 1 is Miami");
 		
-		if (cars[1].equals("BMW")) {System.out.println("Index 1 is BMW"); }
-		else {System.out.println("Index 1 is Mercedes"); }
+		*/
 		
 		
 		
-		//MULTIDIMENSIONAL Arrays
+		String bmw [] = {"M2", "M3", "M340i", "M5", "M8"};
 		
-		int [] [] myNumbers = { {1,2,3,4}, {5,6,7} };
+		System.out.println(bmw.length);
+		bmw[2]= "M4";
+		System.out.println(bmw[2]);
+		
+		
+		for (String x: bmw) {
+			System.out.println(x);
+		}
+		
+		if (bmw[3].equals("X5M")) {System.out.println("Index 3 is X5M");
+		
+		} else System.out.println("index 3 is M5");
+		
+		
+		
+		
+		
+		
+		//MULTI DIMENSIONAL ARRAYS
+		
+		/*	
+		int [][] myNumbers= { {1,2,3,4}, {5,6,7} };
 		
 		System.out.println(myNumbers[0][2]);
-		
-		int y= myNumbers[1][1];
-		System.out.println(y);
-		
+		System.out.println(myNumbers[1][1]);
+		*/
 		
 		
-		String [] fragrances = {"Maison Francis", "Byredo", "YSL", "Diptyque"};
-		System.out.println(fragrances.length);
-		System.out.println(fragrances[3]);
 		
-		fragrances [3]= "Armani Prive";
-		System.out.println(fragrances[3]);
+	//ARRAY LIST can expand and contract in size where as ARRAYS are limited to size
 		
-		if (fragrances[0].equals("Byredo")) {System.out.println( "Index 0 is Byredo"); }
-		else {System.out.println("Index 0 is Maison Francis"); }
+	
 		
-		for (int z=0; z< fragrances.length; z++) {
-			System.out.println(fragrances[z]);
-			
+		
+		ArrayList<String> bmwModels= new ArrayList<>(Arrays.asList("M2","M3","M4","M5", "M8"));
+		
+		
+		
+		System.out.println(bmwModels.get(3));
+		bmwModels.add("X3M");
+		bmwModels.add("X5M");
+		
+		System.out.println(bmwModels.size());
+		
+		bmwModels.set(0,"M2 Comp");
+		
+		System.out.println(bmwModels.get(0));
+		
+		System.out.println(bmwModels);
+		
+		/*
+		 for (String i: bmwModels) {System.out.println(i);
+		 
+		
 		}
+		*/
+		
+		
+		
+		//ARRAY LIST WITH Integers
+		
+		ArrayList<Integer> myNumbers= new ArrayList<Integer>();
+		myNumbers.add(10);
+		myNumbers.add(54);
+		myNumbers.add(30);
+		myNumbers.add(34);
+		myNumbers.add(65);
+		myNumbers.add(71);
+		
+		
+		//System.out.println(myNumbers);
+		
+		Collections.sort(myNumbers);
+		
+		System.out.println(myNumbers);
+		System.out.println(myNumbers.get(1));
+		
+		
+		
+		ArrayList<String> watches= new ArrayList<>(Arrays.asList("Tissot", "IWC", "Movado", "Breitling"));
+		
+		System.out.println(watches);
+		
+		System.out.println(watches.size());
+		
+		System.out.println(watches.get(1));
+		
+		watches.add("Rolex");
+		System.out.println(watches.get(4));
+		
+		watches.remove(2);
+		
+		watches.add("Tag Heaur");
+		
+		System.out.println(watches);
 		
 		
 		
@@ -89,6 +160,42 @@ public class arraysVSArrayList {
 		
 		
 		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	
+		
+		
+		
+		
+		
+		
+			
 		
 		
 		
