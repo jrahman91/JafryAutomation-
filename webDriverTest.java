4f964a3401c0b7dd516bd5ecf17e7f17cf0167d6
jrahman91 <Jafry.Rahman@gmail.com>
2021-07-01 12:17:20 -0400

    added webDriverTest.java

--- webDriverTest.java
+++ webDriverTest.java
@@ -1,91 +1,99 @@
 package javaAutomation;
 
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.Test;
 
 public class webDriverTest {
-
-	public static void main(String[] args) throws InterruptedException {
+	
+	
+	@Test
+	public void webDriver ()
+	
+	{
 		System.setProperty("webdriver.chrome.driver", "/Users/jafry.rahman/Desktop/Drivers/chromedriver");
+		  WebDriver driver = new ChromeDriver();
+		  
+		//Step1   
+		  String URL= "https://www.yahoo.com/";
+		  
+		  driver.get(URL);
+		  
+		  String title= "Yahoo | Mail, Weather, Search, Politics, News, Finance, Sports & Videos";
+		 
+		  Assert.assertEquals(title, driver.getTitle());
+		  
+		  System.out.println(driver.getTitle()); 
+		  
+		 	  
+		 
+		  
+		  
+		  //Step2
+		  
+		  //could not figure out
+		  
+	      List<WebElement> links = driver.findElements(By.className("class=\"ybar-mod-navigation _yb_h0ovh _yb_1acrx\""));
+		  
+		  System.out.println(links.size());	
+		  
+		  
+		  
+		  driver.findElement(By.xpath("//ul[@role=\"presentation\"]")).getSize();
+		  
+		  
+		  
+		  //Step3
+		//could not figure out
+		  
+		  //Step4
 
+		  
+		  driver.findElement(By.xpath("//input[@id=\"ybar-sbq\"]")).sendKeys("New York City");
+		  
+		  driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
+		  
+		  //Step5
+		  
+		  driver.findElement(By.xpath("//input[@id=\"ybar-search\"]")).click();
+		  
+		  //Step6
+		  
+		  WebDriverWait wait = new WebDriverWait(driver,7);
+		  
+		  wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//input[@id=\"ybar-search\"]")));
+		  
+		  //Step7
+		  
+		  driver.findElement(By.xpath("//a[@id=\"ysignin\"]")).click();
+		  
+		  //Step8
+		  //Could not figure out
+		  
+		//Step9
+		  //Could not figure out
+		
+		
+	}
+	
+	@AfterTest
+	public void tearDown()
+	{
 		WebDriver driver = new ChromeDriver();
-		
-		
-		//STEP 1
-		String URL= "https://www.yahoo.com/";
-		driver.get(URL);
-		driver.manage().window().maximize();
-		
-		
-		String title= "Yahoo | Mail, Weather, Search, Politics, News, Finance, Sports & Videos";
-		
-		if (driver.getTitle().equals(title)) {System.out.println("Title page is correct");
-		
-		}
-		System.out.println(driver.getTitle());
-		
-		//STEP 2
-		//STEP 3
-		
-		//STEP 4
-		
-		driver.findElement(By.xpath("//input[@id=\"ybar-sbq\"]")).sendKeys("New York City");
-		Thread.sleep(1000);
-		
-		//STEP 5
-		driver.findElement(By.xpath("//input[@id=\"ybar-search\"]")).click();
-		
-		
-		//STEP 6
-		
-		WebDriverWait wait = new WebDriverWait(driver,7);
-		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//a[@id= \"logo\"]")));
-		
-		//STEP 7
-		
-		driver.findElement(By.xpath("//a[@id=\"ysignin\"]")).click();
-		
-		//STEP 8
-		
-		//test was incorrect. should display TRUE
-		
-		//driver.findElement(By.xpath("//span[@class=\"stay-signed-in checkbox-container\"]")).click();
-		
-		System.out.println(driver.findElement(By.xpath("//span[@class=\"stay-signed-in checkbox-container\"]")).isSelected());
-		
-		//STEP 9
-		
-		//STEP 10
 		driver.close();
 		
 		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-				
-		
-		
+	}
 	
 
-	}
 
 }
